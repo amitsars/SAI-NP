17877ea4173f6156126bd73ce806a1d8c46d44c3
amitsars <amitsarsawt2@gmail.com>
2021-01-24 13:42:41 +0530

    restored files

---
+++ pythonProject-Selenium/main.py
@@ -0,0 +1,10 @@
+import Athlete.registrationAth as regAth
+import Coach.RegistrationCoach as regCoa
+global driver
+
+if __name__ == '__main__':
+    #
+    # expected = settings['Athletes_val_msg']['first_name']
+    # print(expected)
+    regAth.Athletes_tests.registration()
+    regCoa.Coach_test.registration()
