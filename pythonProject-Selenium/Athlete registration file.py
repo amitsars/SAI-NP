f134ae1d3d2b14f30b5afa68867b74a68e37dff1
amitsars <amitsarsawt2@gmail.com>
2021-01-24 14:12:55 +0530

    yodated file

--- pythonProject-Selenium/Athlete registration file.py
+++ pythonProject-Selenium/Athlete registration file.py
@@ -2,7 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import Select
 
 import time
--- pythonProject-Selenium/main.py
+++ pythonProject-Selenium/main.py
@@ -1,5 +1,5 @@
 import Athlete.registrationAth as regAth
-import Coach.RegistrationCoach as regCoa
+# import Coach.RegistrationCoach as regCoa
 global driver
 
 if __name__ == '__main__':
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     # expected = settings['Athletes_val_msg']['first_name']
     # print(expected)
     regAth.Athletes_tests.registration()
-    regCoa.Coach_test.registration()
+    # regCoa.Coach_test.registration()
